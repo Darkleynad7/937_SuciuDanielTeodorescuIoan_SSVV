7f711ca4eee194e7b53115abd048856d26f683e1
DarkLeynad7 <darkleynad7@gmail.com>
2022-03-22 15:02:40 +0200

    Add BVA tests

--- src/main/java/validation/StudentValidator.java
+++ src/main/java/validation/StudentValidator.java
@@ -17,20 +17,32 @@ public class StudentValidator implements Validator<Student> {
         if(entity.getID().equals("")){
             throw new ValidationException("Id empty!");
         }
+        if(entity.getID().length() > 100){
+            throw new ValidationException("Id exceeds size!");
+        }
         if(entity.getNume() == null){
             throw new ValidationException("Nume null!");
         }
         if(entity.getNume().equals("")){
             throw new ValidationException("Nume empty!");
         }
+        if(entity.getNume().length() > 100){
+            throw new ValidationException("Nume exceeds size!");
+        }
         if(entity.getGrupa() < 0) {
             throw new ValidationException("Grupa negativa!");
+        }
+        if(entity.getGrupa() > 1000) {
+            throw new ValidationException("Grupa exceeds size!");
         }
         if(entity.getEmail() == null){
             throw new ValidationException("Email null!");
         }
         if(entity.getEmail().equals("")){
             throw new ValidationException("Email empty!");
+        }
+        if(entity.getEmail().length() > 100){
+            throw new ValidationException("Email exceeds size!");
         }
     }
 }
--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -18,6 +18,7 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Objects;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
@@ -242,5 +243,229 @@ public class StudentTest {
         assert(studentXMLRepo.save(student1) != student1);
         studentXMLRepo.delete("a");
         System.out.println("EC Test 15 completed!\n");
+    }
+
+    @Order(16)
+    @Test
+    public void testAddStudentIdLengthOf0(){
+        Student student1 = new Student("", "a", 2, "a");
+        try{
+            studentXMLRepo.save(student1);
+        }
+        catch (ValidationException v){
+            assert (v.getMessage().equals("Id empty!"));
+        }
+        System.out.println("BVA Test 1 completed!\n");
+    }
+
+    @Order(17)
+    @Test
+    public void testAddStudentIdLengthOf1(){
+        Student student1 = new Student("a", "a", 2, "a");
+        assert(service.addStudent(student1) == null);
+        studentXMLRepo.delete("a");
+        System.out.println("BVA Test 2 completed!\n");
+    }
+
+    @Order(18)
+    @Test
+    public void testAddStudentIdLengthOf2(){
+        Student student1 = new Student("ab", "a", 2, "a");
+        assert(service.addStudent(student1) == null);
+        studentXMLRepo.delete("ab");
+        System.out.println("BVA Test 3 completed!\n");
+    }
+
+    @Order(19)
+    @Test
+    public void testAddStudentIdLengthOf100(){
+        char[] a = new char[100];
+        Arrays.fill(a, 'a');
+        Student student1 = new Student(new String(a), "a", 2, "a");
+        assert(service.addStudent(student1) == null);
+        studentXMLRepo.delete(new String(a));
+        System.out.println("BVA Test 4 completed!\n");
+    }
+
+    @Order(20)
+    @Test
+    public void testAddStudentIdLengthOf101(){
+        char[] a = new char[101];
+        Arrays.fill(a, 'a');
+        Student student1 = new Student(new String(a), "a", 2, "a");
+        try{
+            studentXMLRepo.save(student1);
+        }
+        catch (ValidationException v){
+            assert (v.getMessage().equals("Id exceeds size!"));
+        }
+        System.out.println("BVA Test 5 completed!\n");
+    }
+
+    @Order(21)
+    @Test
+    public void testAddStudentNumeLengthOf0(){
+        Student student1 = new Student("a", "", 2, "a");
+        try{
+            studentXMLRepo.save(student1);
+        }
+        catch (ValidationException v){
+            assert (v.getMessage().equals("Nume empty!"));
+        }
+        System.out.println("BVA Test 6 completed!\n");
+    }
+
+    @Order(22)
+    @Test
+    public void testAddStudentNumeLengthOf1(){
+        Student student1 = new Student("a", "a", 2, "a");
+        assert(service.addStudent(student1) == null);
+        studentXMLRepo.delete("a");
+        System.out.println("BVA Test 7 completed!\n");
+    }
+
+    @Order(23)
+    @Test
+    public void testAddStudentNumeLengthOf2(){
+        Student student1 = new Student("a", "ab", 2, "a");
+        assert(service.addStudent(student1) == null);
+        studentXMLRepo.delete("a");
+        System.out.println("BVA Test 8 completed!\n");
+    }
+
+    @Order(24)
+    @Test
+    public void testAddStudentNumeLengthOf100(){
+        char[] a = new char[100];
+        Arrays.fill(a, 'a');
+        Student student1 = new Student("a", new String(a), 2, "a");
+        assert(service.addStudent(student1) == null);
+        studentXMLRepo.delete("a");
+        System.out.println("BVA Test 9 completed!\n");
+    }
+
+    @Order(25)
+    @Test
+    public void testAddStudentNumeLengthOf101(){
+        char[] a = new char[101];
+        Arrays.fill(a, 'a');
+        Student student1 = new Student("a", new String(a), 2, "a");
+        try{
+            studentXMLRepo.save(student1);
+        }
+        catch (ValidationException v){
+            assert (v.getMessage().equals("Nume exceeds size!"));
+        }
+        System.out.println("BVA Test 10 completed!\n");
+    }
+
+    @Order(26)
+    @Test
+    public void testAddStudentEmailLengthOf0(){
+        Student student1 = new Student("a", "a", 2, "");
+        try{
+            studentXMLRepo.save(student1);
+        }
+        catch (ValidationException v){
+            assert (v.getMessage().equals("Email empty!"));
+        }
+        System.out.println("BVA Test 11 completed!\n");
+    }
+
+    @Order(27)
+    @Test
+    public void testAddStudentEmailLengthOf1(){
+        Student student1 = new Student("a", "a", 2, "a");
+        assert(service.addStudent(student1) == null);
+        studentXMLRepo.delete("a");
+        System.out.println("BVA Test 12 completed!\n");
+    }
+
+    @Order(28)
+    @Test
+    public void testAddStudentEmailLengthOf2(){
+        Student student1 = new Student("a", "a", 2, "ab");
+        assert(service.addStudent(student1) == null);
+        studentXMLRepo.delete("a");
+        System.out.println("BVA Test 13 completed!\n");
+    }
+
+    @Order(29)
+    @Test
+    public void testAddStudentEmailLengthOf100(){
+        char[] a = new char[100];
+        Arrays.fill(a, 'a');
+        Student student1 = new Student("a", "a", 2, new String(a));
+        assert(service.addStudent(student1) == null);
+        studentXMLRepo.delete("a");
+        System.out.println("BVA Test 14 completed!\n");
+    }
+
+    @Order(30)
+    @Test
+    public void testAddStudentEmailLengthOf101(){
+        char[] a = new char[101];
+        Arrays.fill(a, 'a');
+        Student student1 = new Student("a", "a", 2, new String(a));
+        try{
+            studentXMLRepo.save(student1);
+        }
+        catch (ValidationException v){
+            assert (v.getMessage().equals("Email exceeds size!"));
+        }
+        System.out.println("BVA Test 15 completed!\n");
+    }
+
+    @Order(31)
+    @Test
+    public void testAddStudentGrupaIsMinus1(){
+        Student student1 = new Student("a", "a", -1, "");
+        try{
+            studentXMLRepo.save(student1);
+        }
+        catch (ValidationException v){
+            assert (v.getMessage().equals("Grupa negativa!"));
+        }
+        System.out.println("BVA Test 16 completed!\n");
+    }
+
+    @Order(32)
+    @Test
+    public void testAddStudentGrupaIs0(){
+        Student student1 = new Student("a", "a", 0, "a");
+        assert(service.addStudent(student1) == null);
+        studentXMLRepo.delete("a");
+        System.out.println("BVA Test 17 completed!\n");
+    }
+
+    @Order(28)
+    @Test
+    public void testAddStudentGrupaIs1(){
+        Student student1 = new Student("a", "a", 1, "a");
+        assert(service.addStudent(student1) == null);
+        studentXMLRepo.delete("a");
+        System.out.println("BVA Test 18 completed!\n");
+    }
+
+    @Order(29)
+    @Test
+    public void testAddStudentGrupaIs1000(){
+        Student student1 = new Student("a", "a", 1000, "a");
+        assert(service.addStudent(student1) == null);
+        studentXMLRepo.delete("a");
+        System.out.println("BVA Test 19 completed!\n");
+    }
+
+    @Order(30)
+    @Test
+    public void testAddStudentGrupaIs1001(){
+        Student student1 = new Student("a", "a", 1001, "a");
+        try{
+            studentXMLRepo.save(student1);
+        }
+        catch (ValidationException v){
+            assert (v.getMessage().equals("Grupa exceeds size!"));
+        }
+        System.out.println("BVA Test 20 completed!\n");
     }
 }
